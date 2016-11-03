9ddde669ff3514d1590fa8235d0b7e84641bf9ca
Russell Sabia <rjsabia@gmail.com>
2016-11-03 11:01:23 -0700

    reviewed by mentor, fully functional

--- js/app.js
+++ js/app.js
@@ -1,21 +1,42 @@
-function FizzBang (numIn){
-	for( var i = 1; i <= numIn; i++){
-		if ( i % 5 === 0 && i % 3 === 0) {
-	 		$('.fizzbuzz').append("<li>FizzBuzz</li>");
-	    }
-	 	else if ( i % 3 === 0) {
-	 		$('.fizzbuzz').append('<li>Fizz</li>');
-	 	}
-	 	else if ( i % 5 === 0) {
-	 		$('.fizzbuzz').append('<li>Buzz</li>');
-	 	}	
-	 	else {
-	 		$('.fizzbuzz').append('<li>' + i + '</li>');
-	 	}
-	}
+var inputNumber;
+// Below calls the function to gather input from user
+callPrompt();
+
+
+// *************************************************************************
+function callPrompt(){
+  inputNumber = prompt("Please enter a whole number", "Enter number here");
+  // After input recieved, call this function to test if it is valid
+  validateNum(inputNumber);
 }
-
-var inputNumber = prompt("Please enter a whole number", "Enter number here");
-parseInt(inputNumber);
-
-FizzBang(inputNumber);
+// ************************************************************************
+function validateNum(numIn){
+	// Parse coverts string to a number if that is possible
+  parseInt(numIn);
+  // this "if" checks if the parse rurned a number or not, then if it was a decimal
+  if (numIn == NaN || numIn % 1 != 0){
+  	// If it does not pass, calls callPrompt again for new input
+    callPrompt();
+  }
+  else {
+  	// if test passed, call the FizzBang loop function
+    fizzBang(numIn);
+  }
+}
+// ***********************************************************************
+function fizzBang(validNum){
+  for( var i = 1; i <= validNum; i++){
+      if ( i % 5 === 0 && i % 3 === 0) {
+        $('.fizzbuzz').append("<li>FizzBuzz</li>");
+        }
+      else if ( i % 3 === 0) {
+        $('.fizzbuzz').append('<li>Fizz</li>');
+      }
+      else if ( i % 5 === 0) {
+        $('.fizzbuzz').append('<li>Buzz</li>');
+      } 
+      else {
+        $('.fizzbuzz').append('<li>' + i + '</li>');
+      }
+    }
+}
